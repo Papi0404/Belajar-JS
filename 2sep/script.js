e4f75256343035f0be2549e1e5e6fc789b013a58
HYPE AMD <HYPE AMD@HYPEAMD>
2026-09-02 21:04:15 +0700

    push js

---
+++ 2sep/script.js
@@ -0,0 +1,19 @@
+let dataString = 'ini data string';
+console.log(dataString);
+
+let data1 = 'ucup \t berkata apa kabar dunia?';
+console.log(data1);
+let data2 = 'otong berkata \'tetep baik\'';
+console.log(data2);
+let data3 = 'ucup jalan jalan di tepi pantai, \n kereen';
+console.log(data3);
+let data4 = 'saya ingin masuk smk\n pilihan setelah smkn 8 jakarta';
+console.log(data4);
+
+let namadepan = 'otong';
+let namabelakang = 'dipotong';
+let umur = 20;
+let namalengkap = umur + ' ' + namadepan + ' ' + namabelakang;
+console.log(namalengkap);
+let biodata = `nama: ${namadepan}, umur: ${umur}, nama belakang: ${namabelakang}`;
+console.log(biodata);
