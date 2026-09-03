a83aa9688d97db77d18f642cc8313efa168cc811
Papi0404 <papijanuar147@gmail.com>
2026-09-03 14:32:35 +0700

    proyek manipulasi5

--- manipulasi/script.js
+++ manipulasi/script.js
@@ -20,15 +20,15 @@ console.log(kata2, kata2.substring(2));
 let kata3 = "Adam Rais";
 console.log(kata3, kata3.slice(5, 2));
 
-//replace
+//replace and string
 let kata4 = "saya ingin makan nasi goreng";
 console.log(kata4, kata4.replace("nasi goreng", "sate kambing"));
 
-//overide
-let kata5 = "saya ingin makan nasi goreng";
-console.log(kata5, kata5.replace("nasi goreng", "sate kambing").
-replace("saya", "aku"));
+// //overide
+// let kata5 = "saya ingin makan nasi goreng";
+// console.log(kata5, kata5.replace("nasi goreng", "sate kambing").
+// replace("saya", "aku"));
 
-//uppercase
-let kata6 = "saya ingin makan nasi goreng";
-console.log(kata6, kata6.toUpperCase());
+// //uppercase
+// let kata6 = "saya ingin makan nasi goreng";
+// console.log(kata6, kata6.toUpperCase());
