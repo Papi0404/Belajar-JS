1cae971880ef9e5d1838633be9f1e2f558dcee91
Papi0404 <papijanuar147@gmail.com>
2026-09-03 14:14:18 +0700

    proyek manipulaasi

---
+++ manipulasi/script.js
@@ -0,0 +1,23 @@
+let kata = "ABCDEF";
+// console.log(kata.charAt(0));
+console.log(kata, kata.charAt(0));
+console.log(kata, kata.charAt(1));
+console.log(kata, kata.charAt(2));
+
+
+let namalengkap = "dadang sabrudin";
+console.log(namalengkap, namalengkap.indexOf("s"));
+
+let namaDepan ="dadang";
+let namaBelakang = "sabrudin";
+console.log(namaDepan.concat(" ", namaBelakang,"si bedul"));
+
+//memotong kata
+let kata2 = "Darul";
+console.log(kata2, kata2.substring(2));
+
+//slice
+let kata3 = "Adam Rais";
+console.log(kata3, kata3.slice(5, 2));
+
+//ya yaa
