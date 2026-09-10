1127679409d70a7fad49d2a51ee2099bf3b01263
Papi0404 <papijanuar147@gmail.com>
2026-09-10 14:15:52 +0700

    manipulasi16

---
+++ 10 sep_parseInt/lkpd/script.js
@@ -0,0 +1,10 @@
+let harga = "15000";
+let jumlah = "7";
+let diskon = total * 10 / 100;
+
+let total = harga * jumlah;
+let bayar = total - diskon;
+
+console.log("Total harga: Rp" + total);
+console.log("Diskon: Rp" + diskon);
+console.log("Total bayar: Rp" + bayar);
