61fd9a1e5fe0cd56468b23d4b5e29074fe98e82e
Papi0404 <papijanuar147@gmail.com>
2026-09-17 13:41:33 +0700

    manipulasi34

---
+++ Episode_11_LatihanOperator/script.js
@@ -0,0 +1,6 @@
+let aritmatika1,operatorAritmatika,aritmatika2,hasilAritmatika;
+document.getElementById('buttonAritmatika').onclick = function(){
+    aritmatika1 = document.getElementById('aritmatikaangka1').value;
+    aritmatika1 = document.getElementById('aritmatikaangka2').valueNumber;
+    operatorAritmatika = document.getElementById('operatorAritmatika').value;
+}
